d703013d45f915bb1bfd25bae52434f5b3ca3c6b
beligrous <beligrous@gmail.com>
2023-01-17 21:12:29 +0200

    add feedback 2 steps

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,31 +1,56 @@
 import { Component } from 'react';
-import { Button, Container } from './feedback.styled';
+import { Button, Container, Item } from './feedback.styled';
 
-class Feedback extends Component {
+export default class Feedback extends Component {
   state = {
     good: 0,
     neutral: 0,
     bad: 0,
   };
 
+  handleGoodClick = () => {
+    this.setState(prevState => ({ good: prevState.good + 1 }));
+  };
+
+  handleNeutralClick = () => {
+    this.setState(prevState => ({ neutral: prevState.neutral + 1 }));
+  };
+
+  handleBadClick = () => {
+    this.setState(prevState => ({ bad: prevState.bad + 1 }));
+  };
+
+  countPositiveFeedbackPercentage() {
+    const { good } = this.state;
+    return Math.round((good / this.countTotalFeedback()) * 100);
+  }
+
+  countTotalFeedback() {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  }
+
   render() {
+    const { good, neutral, bad } = this.state;
     return (
       <Container>
         <p>Please leave feedback</p>
-        <Button>Good</Button>
-        <Button>Neutral</Button>
-        <Button>Bad</Button>
+        <Button onClick={this.handleGoodClick}>Good</Button>
+        <Button onClick={this.handleNeutralClick}>Neutral</Button>
+        <Button onClick={this.handleBadClick}>Bad</Button>
         <div>
           <p>Statistics</p>
           <ul>
-            <li>Good:</li>
-            <li>Neutral:</li>
-            <li>Bad:</li>
+            <Item>Good: {good}</Item>
+            <Item>Neutral: {neutral}</Item>
+            <Item>Bad: {bad}</Item>
+            <Item>Total: {this.countTotalFeedback()}</Item>
+            <Item>
+              Positive feedback: {this.countPositiveFeedbackPercentage()}%
+            </Item>
           </ul>
         </div>
       </Container>
     );
   }
 }
-
-export default Feedback;
