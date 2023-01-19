8cb539a1aed1a7bc69f200ad2c41e0613537c8f8
beligrous <beligrous@gmail.com>
2023-01-19 19:23:20 +0200

    +

--- src/components/Feedback/feedback.styled.jsx
+++ src/components/Feedback/feedback.styled.jsx
@@ -2,6 +2,7 @@ import styled from '@emotion/styled';
 
 export const Container = styled.div`
   width: 1000px;
+  height: 300px;
   border: 2px solid black;
   text-align: center;
   margin: 150px auto;
